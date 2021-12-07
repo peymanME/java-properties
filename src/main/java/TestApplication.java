a7c7952b12292042ee77024d550c433d4beb4370
peyman.ekhtiar@decsoft.com.pl <peymos7332)>
2021-12-07 17:44:33 +0100

    load properties in java native

---
+++ src/main/java/TestApplication.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author peyman.ekhtiar on 2021-12-07
+ * DECSOFT S.A, Warszawa
+ */
+public class TestApplication {
+    public static void main(String[] args) throws IOException {
+        System.out.println("hello world!!");
+        String resourceName = "config.properties";
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Properties props = new Properties();
+        try(InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
+            props.load(resourceStream);
+            System.out.println(props.getProperty("logger.active"));
+        }
+    }
+}
